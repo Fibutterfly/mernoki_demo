8e199232a17accc6abcef749ad020a0ceccf3ed0
filep <filep.illes@hotmail.com>
2023-03-19 22:05:43 +0100

    becsapos 2

--- becsapos.m
+++ becsapos.m
@@ -5,3 +5,7 @@ sol1 = [double(1); double(2)]
 sol2 = [double(2);double(1)]
 linsolve(coef1,sol1)
 linsolve(coef2,sol2)
+
+coef3 = [double(888445),double(887112);double(887112),double(888445)]
+sol3 = [double(0); double(0)]
+linsolve(coef3, sol3)
