ea9095aa539fddda4db4208181124e79b7d99b2d
filep <filep.illes@hotmail.com>
2023-03-19 20:22:46 +0100

    itt sem jó, doubleval sem

---
+++ becsapos.m
@@ -0,0 +1,7 @@
+format longE
+coef1 = [double(10e-17),double(1);double(1),double(1)]
+coef2 = [double(1),double(1);double(10e-17),double(1)]
+sol1 = [double(1); double(2)]
+sol2 = [double(2);double(1)]
+linsolve(coef1,sol1)
+linsolve(coef2,sol2)
